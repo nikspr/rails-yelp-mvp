f38783f8c7a2dce4182ebaf9df4ec2093807434d
Niklas Springer <me@niklaspringer.xyz>
2023-05-24 14:49:10 +0200

    rubo

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,11 @@
+# frozen_string_literal: true
+
 class ReviewsController < ApplicationController
-  def index
-  end
+  def index; end
 
-  def show
-  end
+  def show; end
 
-  def new
-  end
+  def new; end
 
-  def create
-  end
+  def create; end
 end
